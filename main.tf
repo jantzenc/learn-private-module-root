e543d4b6c1c40a93f3fa48dc97ec227619b52599
Craig Jantzen <craig.jantzen@gmail.com>
2022-07-15 07:30:06 -0500

    3 sites revert

--- main.tf
+++ main.tf
@@ -19,26 +19,10 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-webapp-dot0" {
+module "s3-webapp" {
   source  = "app.terraform.io/jtz-sb1/s3-webapp/aws"
-  version = "1.0.0"
   name        = var.name
   region = var.region
   prefix = var.prefix
-}
-
-module "s3-webapp-dot1" {
-  source  = "app.terraform.io/jtz-sb1/s3-webapp/aws"
-  version = "1.0.1"
-  name        = "${var.name}-dot1"
-  region = var.region
-  prefix = var.prefix
-}
-
-module "s3-webapp-dot3" {
-  source  = "app.terraform.io/jtz-sb1/s3-webapp/aws"
-  version = "1.0.3"
-  name        = "${var.name}-dot3"
-  region = var.region
-  prefix = var.prefix
+  version = "1.0.0"
 }
