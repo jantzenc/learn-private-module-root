524ab2774f406aa9df5bd84c774d58d66c58e6bb
Craig Jantzen <craig.jantzen@gmail.com>
2022-07-15 07:22:16 -0500

    3 sites

--- main.tf
+++ main.tf
@@ -19,10 +19,26 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
+module "s3-webapp-dot0" {
   source  = "app.terraform.io/jtz-sb1/s3-webapp/aws"
+  version = "1.0.0"
   name        = var.name
   region = var.region
   prefix = var.prefix
-  version = "1.0.0"
+}
+
+module "s3-webapp-dot1" {
+  source  = "app.terraform.io/jtz-sb1/s3-webapp/aws"
+  version = "1.0.1"
+  name        = "${var.name}-dot1"
+  region = var.region
+  prefix = var.prefix
+}
+
+module "s3-webapp-dot3" {
+  source  = "app.terraform.io/jtz-sb1/s3-webapp/aws"
+  version = "1.0.3"
+  name        = "${var.name}-dot3"
+  region = var.region
+  prefix = var.prefix
 }
