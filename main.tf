016242acb5f8737626ee7e4257eb308cdd029fc4
Craig Jantzen <craig.jantzen@gmail.com>
2022-07-14 22:10:33 -0500

    fix source syntax

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/jtz-sb1/terraform/s3-webapp/aws"
+  source  = "app.terraform.io/jtz-sb1/s3-webapp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
