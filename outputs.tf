f9c247f19ca00ab142e05d765d4d21fb21767c1d
Craig Jantzen <craig.jantzen@gmail.com>
2022-07-15 07:26:15 -0500

    3 sites

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "website_endpoint" {
-  value = module.s3-webapp.endpoint
+output "website_endpoint_0" {
+  value = module.s3-webapp-dot0.endpoint
+}
+output "website_endpoint_1" {
+  value = module.s3-webapp-dot1.endpoint
+}
+output "website_endpoint_3" {
+  value = module.s3-webapp-dot3.endpoint
 }
